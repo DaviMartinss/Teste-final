a1dc9d008993b64e927c3c84e74535f7ea9dc0a1
Davi Martins <davimartins.dgcm@gmail.com>
2025-04-03 20:20:13 -0300

    configuração django

--- backend/manage.py
+++ backend/manage.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
-
 import os
 import sys
 
 
 def main():
-    """Run administrative tasks."""
+    """"Executar tarefas administrativas."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wallet_management.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "Não foi possível importar o Django. Tem certeza de que ele está instalado e "
+            "disponível na sua variável de ambiente PYTHONPATH? Você "
+            "esqueceu de ativar um ambiente virtual?"
         ) from exc
     execute_from_command_line(sys.argv)
 
